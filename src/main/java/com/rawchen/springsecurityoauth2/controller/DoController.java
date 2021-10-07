1f7e323170f2ba11be617924d0bca7d25fb15804
RawChen <2221999792@qq.com>
2021-10-07 23:39:39 +0800

    Controller

--- src/main/java/com/rawchen/springsecurityoauth2/controller/DoController.java
+++ src/main/java/com/rawchen/springsecurityoauth2/controller/DoController.java
@@ -1,10 +1,9 @@
 package com.rawchen.springsecurityoauth2.controller;
 
 import com.rawchen.springsecurityoauth2.entities.Result;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author RawChen
@@ -13,33 +12,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class DoController {
 
+	@ResponseBody
 	@GetMapping("/admin/hello")
-	public String admin(){
-		return "hello admin";
+	public Result admin(){
+		return Result.ok("hello admin");
 	}
 
+	@ResponseBody
 	@GetMapping("/user/hello")
-	public String user(){
-		return "hello user";
+	public Result user(){
+		return Result.ok("hello user");
 	}
 
+	@ResponseBody
 	@GetMapping("/hello")
 	public Result hello(){
 		return Result.ok("hello");
-	}
-
-	@GetMapping("/login")
-	public Result login(@RequestParam(value = "userId") String id,
-						@RequestParam(value = "password")String password) {
-		if ("1".equals(id) && "1".equals(password)) {
-			return Result.ok("成功登陆");
-		} else {
-			return Result.create(404, "登录失败");
-		}
-
-	}
-
-	public static void main(String[] args) {
-		System.out.println(new BCryptPasswordEncoder().encode("123456"));
 	}
 }
